d0541b9eb9dd55b070c886ea1bec14e61bb42774
PavelNik00 <gh9.111@yandex.ru>
2024-06-25 14:15:43 +0200

    TrackerCategoryStore. Подготовка для работы с Core Data.

--- Tracker/CoreData/Stores/TrackerCategoryStore.swift
+++ Tracker/CoreData/Stores/TrackerCategoryStore.swift
@@ -8,18 +8,65 @@
 import UIKit
 import CoreData
 
+protocol TrackerCategoryDelegate: AnyObject {
+}
+
 final class TrackerCategoryStore: NSObject {
     
+    // синглтон для хранилища категорий
+    static let shared = TrackerCategoryStore()
+    
+    // делегат для уведомления об изменении данных
+    weak var delegate: TrackerCategoryDelegate?
+    
+    // список категорий получаемых из Core Data
+    var categories: [TrackerCategory] {
+        return categories
+    }
+    
+    // контекст Core Data
     private let context: NSManagedObjectContext
 
+    // контроллер для отслеживания изменений в категориях
+    private var categoryFetchedResultsController: NSFetchedResultsController<TrackerCategoryCoreData>?
+    
     // получаем контекст из AppDelegate
     convenience override init() {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Ошибка с инициализацией AppDelegate")
+        }
+        let context = appDelegate.persistentContainer.viewContext
         self.init(context: context)
     }
     
+    // настраиваем контроллер для отслеживания изменений в категориях
     init(context: NSManagedObjectContext) {
         self.context = context
         super.init()
+        let fetchRequest = TrackerCategoryCoreData.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        controller.delegate = self
+        self.categoryFetchedResultsController = controller
+        try? controller.performFetch()
     }
+    
+    private func saveContext() throws {
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch {
+            context.rollback()
+            throw error
+        }
+    }
+}
+
+extension TrackerCategoryStore: NSFetchedResultsControllerDelegate {
+    
 }
