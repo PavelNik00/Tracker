944f47546cb2a44381a976bbd3d363cec8cbe6ee
PavelNik00 <gh9.111@yandex.ru>
2024-06-25 14:19:47 +0200

    TrackerRecordStore. Подготовка для работы с Core Data

--- Tracker/CoreData/Stores/TrackerRecordStore.swift
+++ Tracker/CoreData/Stores/TrackerRecordStore.swift
@@ -10,17 +10,40 @@ import CoreData
 
 final class TrackerRecordStore: NSObject {
     
+    // Синглтон для хранилища записей
+    static let shared = TrackerRecordStore()
+    
+    // Список записей, получаемых из Core Data
+    var records: [TrackerRecord]? {
+        return []
+    }
+    
+    // Контекст Core Data
     private let context: NSManagedObjectContext
 
     // получаем контекст из AppDelegate
     convenience override init() {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Ошибка с инициализацией AppDelegate")
+        }
+        let context = appDelegate.persistentContainer.viewContext
         self.init(context: context)
     }
     
+    // Инициализатор
     init(context: NSManagedObjectContext) {
         self.context = context
         super.init()
     }
     
+    // Сохранение изменений в контексте Core Data
+    private func saveContext() throws {
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch {
+            context.rollback()
+            throw error
+        }
+    }
 }
