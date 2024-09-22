7f45e3b361afe98456e273bcf2ce2db44ee12601
PavelNik00 <gh9.111@yandex.ru>
2024-09-22 14:02:57 +0200

    MVVM. Добавлен файл для CategoryViewModel.

---
+++ Tracker/ViewControllers/Category/CategoryViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryViewModel.swift
+//  Tracker
+//
+//  Created by Pavel Nikipelov on 18.09.2024.
+//
+
+import UIKit
+
+final class CategoryViewModel: NSObject {
+    
+    
+    
+}
